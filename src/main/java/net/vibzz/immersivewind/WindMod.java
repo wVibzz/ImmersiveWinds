42730d1a09864b78c988b99a90598642cc127615
BakinDP <74774853+BakinDP@users.noreply.github.com>
2024-05-16 22:09:51 +0300

    Implemented modern event handler

--- src/main/java/net/vibzz/immersivewind/WindMod.java
+++ src/main/java/net/vibzz/immersivewind/WindMod.java
@@ -2,7 +2,7 @@ package net.vibzz.immersivewind;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.fabricmc.fabric.api.event.world.WorldTickCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.world.World;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,6 +10,7 @@ import org.apache.logging.log4j.Logger;
 public class WindMod implements ModInitializer {
 	public static final String MOD_ID = "windmod";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+	public static volatile int tickCount = 0;
 
 	@Override
 	public void onInitialize() {
@@ -17,11 +18,16 @@ public class WindMod implements ModInitializer {
 		WindManager.initialize();
 
 		// Register server lifecycle events
-		ServerLifecycleEvents.SERVER_STARTED.register(server -> WorldTickCallback.EVENT.register(this::onWorldTick));
+		ServerLifecycleEvents.SERVER_STARTED.register(server -> ServerTickEvents.START_WORLD_TICK.register(this::onWorldTick));
 	}
 
+	
 	public void onWorldTick(World world) {
 		// Update wind based on weather in each world
-		WindManager.updateIfNeeded(world);
+		tickCount += 1; // It's so horrible, but ServerTickEvents is very weird on it's own
+		if(tickCount%3 == 1) {
+			WindManager.updateWeather(world);
+		}
 	}
+
 }
