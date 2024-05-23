97dba3a5e34d95c7c58d22b943e0845d4248c98d
Vibzz <113342610+wVibzz@users.noreply.github.com>
2024-05-22 18:59:50 -0600

    added register sounds

--- src/main/java/net/vibzz/immersivewind/WindMod.java
+++ src/main/java/net/vibzz/immersivewind/WindMod.java
@@ -4,8 +4,10 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.world.World;
+import net.vibzz.immersivewind.sound.ModSounds;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 
 public class WindMod implements ModInitializer {
 	public static final String MOD_ID = "windmod";
@@ -16,18 +18,17 @@ public class WindMod implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("Initializing Wind Mod");
 		WindManager.initialize();
+		ModSounds.registerSounds();
 
 		// Register server lifecycle events
 		ServerLifecycleEvents.SERVER_STARTED.register(server -> ServerTickEvents.START_WORLD_TICK.register(this::onWorldTick));
 	}
 
-	
 	public void onWorldTick(World world) {
 		// Update wind based on weather in each world
-		tickCount += 1; // It's so horrible, but ServerTickEvents is very weird on it's own
+		tickCount += 1; // It's so horrible, but ServerTickEvents is very weird on its own
 		if(tickCount%3 == 1) {
 			WindManager.updateWeather(world);
 		}
 	}
-
 }
