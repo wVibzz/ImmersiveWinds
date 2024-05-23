b9aa3b9329932ca33bc841c03b940afed7cffd45
Vibzz <113342610+wVibzz@users.noreply.github.com>
2024-05-22 18:50:29 -0600

    added logic to play wind sound

--- src/main/java/net/vibzz/immersivewind/sound/ModSounds.java
+++ src/main/java/net/vibzz/immersivewind/sound/ModSounds.java
@@ -1,9 +1,13 @@
 package net.vibzz.immersivewind.sound;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
+import net.vibzz.immersivewind.WindManager;
 import net.vibzz.immersivewind.WindMod;
 
 public class ModSounds {
@@ -18,4 +22,21 @@ public class ModSounds {
     public static void registerSounds() {
         WindMod.LOGGER.info("Sounds are initialized");
     }
+
+    public static void playWindSound(World world, PlayerEntity player) {
+        if (player == null) {
+            System.out.println("Player is null, cannot play sound.");
+            return;
+        }
+
+        float volume = WindManager.calculateWindVolume(world);
+        double x = player.getX();
+        double y = player.getY();
+        double z = player.getZ();
+
+        System.out.println("Attempting to play wind sound at: " + x + ", " + y + ", " + z + " with volume: " + volume);
+        world.playSound(null, x, y, z, WIND_SOUND, SoundCategory.AMBIENT, volume, 1.0f);
+        System.out.println("Wind sound played for player: " + player.getName().getString());
+    }
+
 }
