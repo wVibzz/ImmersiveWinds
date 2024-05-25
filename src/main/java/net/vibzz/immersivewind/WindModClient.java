af374d754e2deb07e490af2cca473397abfdbb6d
Vibzz <113342610+wVibzz@users.noreply.github.com>
2024-05-24 23:59:22 -0600

    fixed class to WindModClient

--- src/main/java/net/vibzz/immersivewind/WindModClient.java
+++ src/main/java/net/vibzz/immersivewind/WindModClient.java
@@ -3,7 +3,7 @@ package net.vibzz.immersivewind;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
-public class WindEffects implements ClientModInitializer {
+public class WindModClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
